507fc0e6f6f49ca1474e6e5acf78313dfdcc17b5
Ana <anagonm@gmail.com>
2022-10-13 07:41:16 -0400

    Initial work for css/styles and splits some components

--- src/components/widgets/DailyWidget.js
+++ src/components/widgets/DailyWidget.js
@@ -3,7 +3,7 @@ import React from "react";
 const DailyWidget = () => {
   return (
     <div className="daily">
-      <img src="https://openweathermap.org/img/wn/10d@2x.png" />
+      <img src="https://openweathermap.org/img/wn/10d@2x.png" alt=""/>
       <p>Sun</p>
       <p>28º</p>
     </div>
