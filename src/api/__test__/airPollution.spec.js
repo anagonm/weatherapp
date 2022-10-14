c8362dc2a9a85e2ae0ab0cc1e5f3446b946ca341
Ana <anagonm@gmail.com>
2022-10-14 08:26:37 -0400

    Adds tests for the airPollution api

---
+++ src/api/__test__/airPollution.spec.js
@@ -0,0 +1,59 @@
+import fetchMock from "jest-fetch-mock";
+import { ERROR_INVALID_LAT_LON, getAirPollutionByLatLon } from "../airPollution";
+
+fetchMock.enableMocks();
+
+describe("AirPolutionAPI", () => {
+  // Variables and function params for a success case
+  const latitude = -80.1747;
+  const longitude = 25.9445;
+
+  const fetchAirPolutionMockedResponse = {
+    "coord": {
+      "lon": -80.1747,
+      "lat": 25.9445
+    },
+    "list": [
+      {
+        "main": {
+          "aqi": 1
+        },
+        "components": {
+          "co": 387.19,
+          "no": 19,
+          "no2": 14.74,
+          "o3": 0.04,
+          "so2": 2.53,
+          "pm2_5": 5.84,
+          "pm10": 7.85,
+          "nh3": 1.03
+        },
+        "dt": 1665748356
+      }
+    ]
+  };
+
+  beforeEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  it("returns data from the OpenWeather API", async () => {
+    fetch.mockResponseOnce(JSON.stringify(fetchAirPolutionMockedResponse));
+    const openWeatherAPIResponse = await getAirPollutionByLatLon(latitude, longitude);
+    expect(openWeatherAPIResponse).toEqual(fetchAirPolutionMockedResponse);
+  })
+
+  it("returns and error if invalid data", async () => {
+    fetch.mockResponseOnce(JSON.stringify(fetchAirPolutionMockedResponse));
+
+    let result = null;
+
+    try {
+      await getAirPollutionByLatLon(); // Missing params
+    } catch(e) {
+      result = e
+    }
+
+    expect(result).toMatch(ERROR_INVALID_LAT_LON);
+  })
+})
--- src/api/airPollution.js
+++ src/api/airPollution.js
@@ -1,6 +1,12 @@
 import { BASE_URL_WEATHER, fetchData } from "./common";
 
+export const ERROR_INVALID_LAT_LON = "Invalid latitude or longitude, make sure your browser support Javascript and HTML";
+
 export const getAirPollutionByLatLon = async (lat_, lon_) => {
+  if (!lat_ || !lon_) {
+    throw ERROR_INVALID_LAT_LON;
+  }
+
   const baseUrl = BASE_URL_WEATHER + "/air_pollution";
   const params = `lat=${lat_}&lon=${lon_}`;
   return await fetchData(baseUrl, params);
